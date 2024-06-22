7011a683117dfeb1c902b18323dd6d88d0307350
NibakureJosee <joseenibakure@gmail.com>
2024-06-23 01:07:26 +0200

    first commit

---
+++ src/main/java/com/example/binarysupermarket/BinarySupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.binarysupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarySupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarySupermarketApplication.class, args);
+	}
+
+}
